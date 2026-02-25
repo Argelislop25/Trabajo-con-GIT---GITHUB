8e660f72220b5e1a0bd5302d03f550a7737b6d21
Argelislop25 <lopez.argelis002@icloud.com>
2026-02-24 20:23:31 -0500

    multiplicacion elementos lista

--- Index.py
+++ Index.py
@@ -1,2 +1,16 @@
 lista = [1, 2, 3, 4, 5, 9, 89]
 print(f'la longitud de la lista es : {len(lista)}')
+
+suma = 0
+for i in lista :
+    suma += i 
+
+print(f'\nla suma de los valores de la lista es : {suma}')
+
+multiplicacion = 1
+for i in lista :
+    multiplicacion *=i
+
+print(f'\nla multiplicacion de los elementos lista : {multiplicacion}')
+
+
